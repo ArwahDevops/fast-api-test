330b715d7f13c6439ba79bcb3a42420d4c97d821
Riki Permana <arwahdevops@gmail.com>
2021-10-08 11:16:24 +0700

    test

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import uvicorn
-from fastapi import FastAPI
+import socket
+from fastapi import FastAPI, request
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return "This is an example wsgi app served from {} to {}".format(socket.gethostname(), request.remote_addr)
